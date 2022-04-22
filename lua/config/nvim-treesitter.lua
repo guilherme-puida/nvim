df36c56bca5b9f9e2e325889715fcbc38edf3277
Guilherme Puida Moreira <guilhermepmoreira00@gmail.com>
2022-04-22 00:10:08 -0300

    Added languages to treesitter

--- lua/config/nvim-treesitter.lua
+++ lua/config/nvim-treesitter.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = {"python", "javascript", "go", "rust", "lua", "c", "typescript"},
+  ensure_installed = {"python", "javascript", "go", "gomod", "gowork", "rust", "lua", "c", "typescript"},
   sync_install = false,
   highlight = {
     enable = true,
