f5939820c6192bae5dc1b000283b1aff20134879
Guilherme Puida <guilherme@puida.xyz>
2024-03-22 16:22:14 -0300

    add neogit

--- lua/rc/plugins/git.lua
+++ lua/rc/plugins/git.lua
@@ -2,5 +2,17 @@ return {
   {
     'lewis6991/gitsigns.nvim',
     config = true,
-  }
+  },
+  {
+    'NeogitOrg/neogit',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'sindrets/diffview.nvim',
+    },
+    config = true,
+    keys = {
+      { '<leader>gg', '<cmd>Neogit kind=split<cr>', 'git panel (split)' },
+      { '<leader>gG', '<cmd>Neogit kind=split<cr>', 'git panel' },
+    }
+  },
 }
