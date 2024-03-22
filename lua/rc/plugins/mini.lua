e8b6ba85a73be24bdcc54401becb60cf9fc3445b
Guilherme Puida <guilherme@puida.xyz>
2024-03-22 09:30:13 -0300

    add mini.files

--- lua/rc/plugins/mini.lua
+++ lua/rc/plugins/mini.lua
@@ -17,4 +17,18 @@ return {
       set_vim_settings = false,
     },
   },
+  {
+    'echasnovski/mini.files',
+    dependencies = {
+      'nvim-tree/nvim-web-devicons',
+    },
+    opts = {
+      windows = {
+        max_number = 3,
+      },
+    },
+    keys = {
+      { '<leader>ee', '<cmd>lua MiniFiles.open()<cr>', desc = 'open directory' },
+    },
+  },
 }
